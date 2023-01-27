62e7e193f03ddb38abeea8a9d08ef48c9f062e32
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-01-27 12:51:38 +0100

    added / at the end

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -12,7 +12,7 @@ const credentials = {
   auth_uri: "https://acounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://Bjaguar71.github.io/MeetApp", "http://localhost:3000"],
+  redirect_uris: ["https://Bjaguar71.github.io/MeetApp/", "http://localhost:3000"],
   javascript_origins: ["https://Bjaguar71.github.io", "http://localhost:3000"],
 };
 
