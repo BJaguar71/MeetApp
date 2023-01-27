a4c86aae84fd7eb3a1474fcffababd1668edd8a7
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-01-27 18:04:39 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         This is an app for finding events in the city to join!
+        I hope you enjoy it!
         </p>
         <a
           className="App-link"
