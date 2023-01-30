638b4603f228e2e985dc779251403dfc57394a3a
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-01-30 13:48:05 +0100

    imported necessary things

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+// import react to could test React component
+import React from "react";
+import { shallow } from "enzyme";
+// import component to test it
+import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch";
+
