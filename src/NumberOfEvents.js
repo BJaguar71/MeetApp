5670a8d1885e0ebf90c3cf5c37820153119abed3
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-01-30 18:08:49 +0100

    created a component to shows the specific number of events
    including a number input

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+
+class NumberOfEvents extends Component {
+  // initial state when user doesn't specify the number of events
+  constructor() {
+    super();
+    this.state = {
+      numberOfEvents: 32,
+    }
+  }
+
+  // when user chnages the number of events to be shown
+  componentDidMount() {
+    // I don't know what to write here 
+  }
+
+  render() {
+    return (
+      <div className="NumberOfEvents">
+        <input
+          className="number"
+          type="number"
+          value={this.state.numberOfEvents}  
+        />
+        <h3>Number of Events:</h3>
+        <ul className="event suggestions">
+        </ul>
+      </div>
+    );
+  };
+}
+
+export default NumberOfEvents;
