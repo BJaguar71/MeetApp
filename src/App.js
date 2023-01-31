c2ea1954b119b175801546518b0d3f5de1dd7548
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-01-31 15:00:07 +0100

    small modification

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-import Event from "./Event";
 import NumberOfEvents from "./NumberOfEvents";
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
-      <Event />
       <NumberOfEvents />
+      <EventList />
     </div>
   );
 }
