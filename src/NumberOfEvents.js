55e87f50d6163f5b05e7892a00518998f3034d4e
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-03 15:01:39 +0100

    updated the numberOfEvents component so that when user specify the number outside the range 1 to 32 an alert will be shown

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { ErrorAlert } from "./Alert";
+
 class NumberOfEvents extends Component {
   // initial state when user doesn't specify the number of events
 
   state = {
     numberOfEvents: 32,
+    infoText: "" 
   };
   // when user chnages the number of events to be shown
   // we need to have function which handle the changing value of the input
   changeNum = (value) => {
-    if (0 < value && value <= 32) {
-      this.setState({ numberOfEvents: value });
-      this.props.updateNumberOfEvents(value);
+    if (value <= 0 || value > 32) {
+      this.setState({
+        numberOfEvents: value,
+        infoText: "Please choose a number between 1 and 32." 
+      });
+    } else {
+      return this.setState({
+        numberOfEvents: value,
+        infoText: ""
+      });
     }
+    // if (0 < value && value <= 32) {
+    //   this.setState({ numberOfEvents: value });
+    //   this.props.updateNumberOfEvents(value);
+    // }
   };
 
   componentDidMount() {
-    this.setState({ numberOfEvents: this.props.num || 32 });
+    this.setState({ 
+      numberOfEvents: this.props.num || 32,
+      infoText: ""
+    });
   }
 
   render() {
@@ -33,6 +50,7 @@ class NumberOfEvents extends Component {
             value={numberOfEvents}
             onChange={(event) => this.changeNum(event.target.value)}
           ></input>
+          <ErrorAlert text={this.state.infoText}/>
         </label>
       </div>
     );
