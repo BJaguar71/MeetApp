18070d684b9e7392de9dcff1141a8835f1709bc6
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-03 18:17:31 +0100

    updated index.js by adding serviceworker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import reportWebVitals from "./reportWebVitals";
 
 import * as atatus from 'atatus-spa';
@@ -14,6 +17,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
