04ddc68226bb6601df724307b2267f6a8cf78d1f
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-04 21:34:06 +0100

    added welcome screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NumberOfEvents from "./NumberOfEvents";
 import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import "./nprogress.css";
 import { WarningAlert } from "./Alert";
+import WelcomeScreen from './WelcomeScreen';
 
 
 class App extends Component {
@@ -41,20 +42,32 @@ class App extends Component {
   // create componentDidMount to make API call and save the initial data to state:
 
   async componentDidMount() {
-    if(navigator.onLine === false) {
+    if (navigator.onLine === false) {
       this.setState({
         infoText: "You are offline! Check your internet connection to see upcoming events"
       });
     }
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events),
-        });
-      }
-    });
+
+    // get the token from localStorage
+    const accessToken = localStorage.getItem('access_token');
+    // verify the token 
+    // If there’s an error in the object returned by checkToken(), the variable isTokenValid will be assigned with the value false; otherwise, it will be true.
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
   }
   // use componentWillUnmount to avoide the component to be unmounted before the API call is finished
 
@@ -66,9 +79,11 @@ class App extends Component {
     // assigned the state into a var 'events' to simplify the value of 'event' prop
     const { events, locations } = this.state;
 
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
     return (
       <div className="App">
-        <WarningAlert text={this.state.infoText}/>
+        <WarningAlert text={this.state.infoText} />
         <div className="filters">
           <CitySearch
             locations={locations}
@@ -80,6 +95,8 @@ class App extends Component {
             num={this.state.numberOfEvents}
             updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
           />
+          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+            getAccessToken={() => { getAccessToken() }} />
         </div>
         <EventList events={events} />
       </div>
