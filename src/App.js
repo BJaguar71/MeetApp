ddd75a74292fde5b9eac12d02524f9ed37b8b6d9
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-05 09:37:28 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,36 @@ class App extends Component {
     infoText: ""
   };
 
+  async componentDidMount() {
+    this.mounted = true;
+    if (!navigator.onLine) {
+      this.setState({
+        infoText: "You are offline! Check your internet connection to see upcoming events"
+      });
+    }
+
+    // get the token from localStorage
+    const accessToken = localStorage.getItem('access_token');
+    // verify the token 
+    // If there’s an error in the object returned by checkToken(), the variable isTokenValid will be assigned with the value false; otherwise, it will be true.
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events)
+          });
+        }
+      });
+    }
+  }
+
   updateEvents = (location) => {
     getEvents().then((events) => {
       //  filteres events that their location is equal to selected location
@@ -41,31 +71,7 @@ class App extends Component {
   }
   // create componentDidMount to make API call and save the initial data to state:
 
-  async componentDidMount() {
-    if (navigator.onLine === false) {
-      this.setState({
-        infoText: "You are offline! Check your internet connection to see upcoming events"
-      });
-    }
-    this.mounted = true;
 
-    // get the token from localStorage
-    const accessToken = localStorage.getItem('access_token');
-    // verify the token 
-    // If there’s an error in the object returned by checkToken(), the variable isTokenValid will be assigned with the value false; otherwise, it will be true.
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-      true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
-  }
   // use componentWillUnmount to avoide the component to be unmounted before the API call is finished
 
   componentWillUnmount() {
@@ -73,11 +79,13 @@ class App extends Component {
   }
 
   render() {
-    // assigned the state into a var 'events' to simplify the value of 'event' prop
-    const { events, locations } = this.state;
 
     if (this.state.showWelcomeScreen === undefined) return <div
       className="App" />
+
+    // assigned the state into a var 'events' to simplify the value of 'event' prop
+    const { events, locations, showWelcomeScreen } = this.state;
+
     return (
       <div className="App">
         <WarningAlert text={this.state.infoText} />
@@ -92,10 +100,10 @@ class App extends Component {
             num={this.state.numberOfEvents}
             updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
           />
-          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-            getAccessToken={() => { getAccessToken() }} />
         </div>
         <EventList events={events} />
+        <WelcomeScreen showWelcomeScreen={false}
+          getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
