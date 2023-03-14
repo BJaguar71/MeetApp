bd67e824351e822093444f2daed9c340bfe0a83c
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-14 11:37:58 +0100

    rendered elements related to the charts

--- src/App.js
+++ src/App.js
@@ -125,6 +125,23 @@ class App extends Component {
             updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
           />
         </div>
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={events} />
         <WelcomeScreen showWelcomeScreen={showWelcomeScreen}
           getAccessToken={() => { getAccessToken() }} />
